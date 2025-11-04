4fd17a2a142b4910c19f5db2750d54eecfb3b3a3
youseffai <altyoxox@gmail.com>
2025-11-04 19:47:42 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,24 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+  if num1>num2 and num1>num3:
+    return num1
+  if num2>num3 and num2>num3:
+    return num2
+  if num1>=num2>=num3 or num1>=num3>=num2:
+    return num1
+  if num2>=num1>=num3 or num2>=num3>=num1:
+    return num2
+  else:
+    return num3
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    total=num1+num2+num3
+    n=3
+    mean=total/3
+    return mean
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    variance = ((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3
+    std_dev = math.sqrt(variance)
+    return mean, std_dev
 
